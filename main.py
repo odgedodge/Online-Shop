61b26d03f10d2d4ff064f77513a2e8924d58d5be
Oliver <jamesonosa@cardiff.ac.uk>
2024-04-30 16:20:49 +0100

    currently an issue due to not running from venv due to SQL alchemy updates, so database is remade every runtime for now, will fix later. Made so the product is added to the cart, not a string value, and quanitiy is updated

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from wtforms.validators import DataRequired, Length, NumberRange
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
+products = [
+    { "name": "Singer Sewing Machine M2105", "price": 149.99, "description": "Lightweight, beginner sewing machine with one dial operation and 4 step buttonhole stitching.", "image_filename" : "SingerM1205SewingMachine.jpg", "environmental_impact": "Sewing machines have environmental impacts as electronic devices, in that they do use electricity, as well as them adding to e-waste if not disposed of correctly. They are also made of plastics and polymers, so they should be disposed of with the proper care. However, moving to making your own clothes for yourself or friends is far better than participating in fast fashion, as well as being.   "},
+    { "name": "Beginner Sewing Kit", "price": 14.99, "description": "Sewing kit containing 12 different thread colours, 1 needle set, 1 seam ripper, 1 thimble, 1 pair of sewing scissors, 40 pins and 1 sewing cushion.", "image_filename" : "BeginnerSewingKit.jpg",  "environmental_impact": "This kit has a lot of little plastic parts and some aspects that may not be used, so it can be environmentally wasteful, so make sure that the aspects of this kit are necessary for your project. Take the proper care to recycling plastic parts. The overall environmental impact is poor due to the large amount of plastic involved."},
+    { "name": "Fabric Scissors", "price": 15.99, "description": '8" Heavy duty, sharp scissors with a case.', "image_filename" : "Scissors.jpg" ,  "environmental_impact": "These scissors should last a long time, which is why we give a 10-year warranty for repairs. Look to sharpen these before seeking a replacement, and when disposing keep note of local metal and sharp handling policies. The making of these use a large amount of energy and water, so it is in the best interest of the environment to keep them functioning through repair and sharpening rather than to replace them. Make sure to only use this on fabric for best longevity – use on paper can cause bluntness."},
+    { "name": "Simplicity Vintage Jiffy Dress Sewing Pattern S9594", "price": 10.99, "description": "Vintage dress sewing pattern. For sizes 6 -14. ", "image_filename" : "simplicity-sewing-pattern-s9594-jiffy-dress.webp",  "environmental_impact": "Sewing patterns are made from paper, usually thin, tissue paper, as the Simplicity Vintage Jiffy Dress Sewing Papper S9594 is. This has a low environmental impact, however mass production of paper can cause issues with deforestation, so when disposing of this item make sure to recycle appropriately. Making your own clothes can also be beneficial for the environment, as we move into a period where fast fashion is more criticised for its negative environmental impact. "}
+]
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "top secret password don't tell anyone this"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
@@ -18,6 +26,7 @@ class Product(db.Model):
     price = db.Column(db.Float)
     description = db.Column(db.Text)
     image_name = db.Column(db.String(60))
+    environmental_impact = db.Column(db.String(600))
     
     # in order to display a products details
     def serialise(self):
@@ -25,11 +34,13 @@ class Product(db.Model):
                 "name": self.name,
                 "price": self.price,
                 "description": self.description,
-                "image_name": self.image_name}
+                "image_name": self.image_name,
+                "environmental_impact": self.environmental_impact}
 
 class CartForm(FlaskForm):
-    cart = IntegerField('Add to cart: ', [DataRequired(), NumberRange(1)])
-    submit = SubmitField('Submit')
+    cart = IntegerField('Quantity: ', [DataRequired(), NumberRange(min=1)], 
+                        render_kw={"type": "number", "min": "1"})
+    submit = SubmitField('Add to Cart')
 
 @app.route('/')
 def main_page():
@@ -41,24 +52,52 @@ def main_page():
     products = Product.query.all()
     return render_template('main.html', len = len(products), products = products)
 
-@app.route('/product/<int:productId>', methods=['GET','POST'])
-def single_product_page(productId):
+@app.route('/product/<int:product_id>', methods=['GET','POST'])
+def single_product_page(product_id):
+    product = Product.query.get_or_404(product_id)
     
-    products = Product.query.all()  
-    
-    form = CartForm(cart=1)
+    form = CartForm()
     
     if form.validate_on_submit():
-        # Add the products to cart if an product is added
-        session['cart'] += [f'{products[productId].name} - £{products[productId].price} - Quantity: {form.cart.data}']
+        quantity = form.cart.data
         
-        return render_template('single_product_confirmation.html', product = products[productId], number_for_cart = form.cart.data)
+        item_in_cart = False
+        for item in session['cart']:
+            if item["product_id"] == product_id:
+                item["quantity"] += quantity
+                item_in_cart = True
+                break
+        if not item_in_cart:
+            session['cart'].append({"product_id": product_id, "quantity": quantity})
+        # Explicitly update the session object
+        session.modified = True
+        return render_template('single_product_confirmation.html', product=product, quantity=quantity)
     else:
-        return render_template('single_product.html', product = products[productId], form = form)
+        return render_template('single_product.html', product=product, form=form)
     
 @app.route('/cart')
 def cart_page():
-    return render_template('cart.html', cart = session['cart'])
+    cart = session.get('cart', [])
+    products_in_cart = []
+    for item in cart:
+        product_id = item["product_id"]
+        product = Product.query.get(product_id)
+        if product:
+            products_in_cart.append({"product": product, "quantity": item["quantity"]})
+    return render_template('cart.html', cart=products_in_cart)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    ## Currently a bug where cannot run the program using a db created in database file: must fix.
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        
+        # enumerate to manually set indexes to index at 0
+        for index, product in enumerate(products):
+            new_product = Product(id=index, name = product["name"], price = product["price"], description = product["description"], image_name = product["image_filename"])
+            db.session.add(new_product)
+            
+        db.session.commit()
+        
+        app.run(debug=True)
