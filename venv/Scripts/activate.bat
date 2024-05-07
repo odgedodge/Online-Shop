9d3f5e8a71cd941a76e3f8bfe87e3044920a0577
Oliver <jamesonosa@cardiff.ac.uk>
2024-05-07 17:24:30 +0100

    remade venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\olive\OneDrive\Documents\Uni\Computer Science\Repositories\cm1102-online-shop\venv
+set VIRTUAL_ENV=C:\Users\olive\OneDrive\Documents\Uni\Computer Science\Repositories\cm1102-online-shop\cm1102-online-shop\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
